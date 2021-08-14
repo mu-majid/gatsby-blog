85197b7802889380b996460a726fd8855f7a7246
Muhammad Majid <mmajidkamel@gmail.com>
2021-08-14 22:29:42 +0200

    index page with correct query and styled components

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,47 @@
  * See: https://www.gatsbyjs.com/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+// Hijacking the build and attaching a slug field to MD nodes
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` }) // Slug is link to blog post
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
+
+// Create page for each post from the respective MD file
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  // Remember () is required when using gql with Nodejs
+  return graphql(`
+     {
+       allMarkdownRemark {
+         edges {
+           node {
+             fields {
+               slug
+             }
+           }
+         }
+       }
+     }
+   `).then(result => {
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve(`./src/templates/blog-post.js`),
+        context: {
+          slug: node.fields.slug,
+        },
+      })
+    })
+  })
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,49 @@
 import * as React from "react"
+import styled from "styled-components"
 import { graphql, Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
-import Seo from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)
+const Title = styled.h1`
+  display: inline-block;
+`
 
-export default IndexPage
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  &:hover {
+    color: #1dcaff;
+  }
+`
+
+const BlogLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+`
+
+const BlogBody = styled.div`
+  margin-bottom: 50px;
+`
+
+export default ({ data }) => {
+  return (
+    <Layout>
+      <div>
+        <Title>Thoughts by Majid</Title>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <BlogBody key={node.id}>
+            <BlogLink to={node.fields.slug}>
+              <BlogTitle>
+                {node.frontmatter.title} <span>— {node.frontmatter.date}</span>
+              </BlogTitle>
+            </BlogLink>
+            <p>{node.frontmatter.description || node.excerpt}</p>
+          </BlogBody>
+        ))}
+      </div>
+    </Layout>
+  )
+}
 
 export const query = graphql`
   query {
