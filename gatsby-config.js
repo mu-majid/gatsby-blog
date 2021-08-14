de03bd41039da8360394438bb85bff63781a9044
Muhammad Majid <mmajidkamel@gmail.com>
2021-08-14 23:08:07 +0200

    adding new blog post for react basics

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,19 @@ module.exports = {
         path: `${__dirname}/src/markdown-pages`,
       },
     },
-    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 800,
+            },
+          },
+        ],
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-sharp`,
