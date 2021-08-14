053899d3dc767658cb8604910952ad8ab482ef9f
Muhammad Majid <mmajidkamel@gmail.com>
2021-08-14 23:01:25 +0200

    change blog title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Testing Blog`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Dev Notes`,
+    description: `Notes I took when taking courses or reading books.`,
+    author: `@Mu-Majid`,
+    siteUrl: `https://github.com/mu-majid`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
